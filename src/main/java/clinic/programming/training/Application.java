3343e3bfbdcf350520194690deb33abe86a4ab1c
Nikita jain <nikita0730.cse19@chitkara.edu.in>
2022-11-15 11:45:50 +0530

    Adding dummy file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,17 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application{
+
+    public void greet(){
+        List<String> greeting=new ArrayList<>();
+        greeting.add("Hello");
+        for(String g:greeting){
+            System.out.println("Greeting: "+g);
+        }
+    }
 
     public Application(){
         System.out.println("Inside application");
@@ -9,6 +20,7 @@ public class Application{
     public static void main(String[] args){
         System.out.println("Starting application");
         Application ap=new Application();
+        ap.greet();
     }
 
 }
