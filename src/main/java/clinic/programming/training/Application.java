6d98c93d492a508f14ed96ac77098f4db329d99a
Nikita jain <nikita0730.cse19@chitkara.edu.in>
2022-11-15 10:21:44 +0530

    Adding Application Java Source

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,14 @@
+package clinic.programming.training;
+
+public class Application{
+
+    public Application(){
+        System.out.println("Inside application");
+    }
+
+    public static void main(String[] args){
+        System.out.println("Starting application");
+        Application ap=new Application();
+    }
+
+}
